738e64f4b7976ede6137078defac987d72b95978
TMSulik <shulich@hotmail.com>
2019-09-20 18:45:14 -0700

    revert back to local version

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,22 @@ var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "MSQabraxa$7",
-//     database: "burgers_db"
-//   });
 } else {
   connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-02.cleardb.net",
+    host: "localhost",
     port: 3306,
-    user: "b830d517332ea6",
-    password: "43028d09",
-    database: "heroku_1a9a89e61f1406a"
+    user: "root",
+    password: "MSQabraxa$7",
+    database: "burgers_db"
   });
+// } else {
+//   connection = mysql.createConnection({
+//     host: "us-cdbr-iron-east-02.cleardb.net",
+//     port: 3306,
+//     user: "b830d517332ea6",
+//     password: "43028d09",
+//     database: "heroku_1a9a89e61f1406a"
+//   });
 
 }
 
